c4e871ef47fc6ddca0ce43f3c637cec87f9d9b16
JigarNSolanki <38855407+JigarNSolanki@users.noreply.github.com>
2018-11-06 23:54:28 +0530

    Create LeftChild.cpp

---
+++ LeftChild.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<list>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* newnode(int key){
+    Node* temp = new Node;
+    temp->data = key;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+
+int leftchild(Node* node){
+    int x=0;
+    if((node->left->left == NULL) && (node->left->right == NULL)){
+            if(node->right != NULL)
+                x = node->left->data;
+            if((node->right->left == NULL) && (node->right->right == NULL))
+                return x;
+            else
+            return x + leftchild(node->right);
+    }
+    return x + leftchild(node->left) + leftchild(node->right); 
+}
+
+int main(){
+    int sum = 0;
+    Node* root = new Node;
+    root-> data = 1;
+    root->left = newnode(3);
+    root->right = newnode(5);
+    root->left->left = newnode(7);
+    root->left->right = newnode(8);
+    root->right->left = newnode(6);
+    root->right->right = newnode(3);
+    cout<<leftchild(root)<<endl;
+    return 0;
+}
